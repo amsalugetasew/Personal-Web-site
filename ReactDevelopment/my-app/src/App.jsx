c32d48c56e1cb89547aedaf82d1c018ecd8fe484
Getasew <amsalugetasew12@gmail.com>
2022-03-30 22:54:12 -0700

    initial commit

---
+++ ReactDevelopment/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+const Container =styled.div`
+    height: 100vh;
+    background-color: red;
+    `
+
+function App() {
+  return (
+    <Container>
+      Hello World!
+    </Container>
+  );
+}
+
+export default App;
