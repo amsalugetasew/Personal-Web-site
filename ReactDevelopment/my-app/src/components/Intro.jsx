045c0d3bb77903b9739b7a26dfec17b07acc66c5
Getasew <amsalugetasew12@gmail.com>
2022-04-11 04:46:19 -0700

    last commit

---
+++ ReactDevelopment/my-app/src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Container=styled.div`
+    height: clac(100vh - 50px)
+`;
+const Intro = () => {
+  return (
+    <div>Intro</div>
+  )
+}
+
+export default Intro
--- ReactDevelopment/my-app/src/components/NavBar.jsx
+++ ReactDevelopment/my-app/src/components/NavBar.jsx
@@ -6,6 +6,7 @@ height:50px;
 const Wrapper=styled.div`
 padding: 10px 20px;
 display: flex;
+align-items: center;
 justify-content:space-between;
 // justify-content:space-around;
 // justify-content:center;
@@ -13,17 +14,47 @@ justify-content:space-between;
 `;
 const Left=styled.div`
 width:60%;
+display: flex;
+align-items: center;
+justify-content:space-between;
 `;
 const Logo=styled.h1`
 font-weight: bold;
 text-decoration: underline crimson;
-`
+`;
+const Menu=styled.ul`
+display: flex;
+list-style: none;
+`;
+const MenuItem=styled.li`
+margin-right: 30px;
+font-size: 20px;
+font-width: bold;
+color: gray;
+`;
+const Button=styled.button`
+border: 2px solid white;
+padding: 10px 15px;
+background-color: crimson;
+color: white; 
+font-width: bold;
+border-radius: 10px;
+cursor: pointer;
+`;
 const NavBar = () => {
   return (
     <Container>
       <Wrapper>
        <span><Logo>Agency</Logo></span> 
-       <span>Button</span> 
+       <Left><Menu>
+       <MenuItem>Home</MenuItem>
+       <MenuItem>Features</MenuItem>
+       <MenuItem>Services</MenuItem>
+       <MenuItem>Pricing</MenuItem>
+       <MenuItem>Contact</MenuItem>
+       </Menu>
+       </Left>
+       <Button>JOIN NOW</Button> 
     </Wrapper> 
     </Container>
   )
